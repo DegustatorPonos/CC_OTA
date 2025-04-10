80beebfa65d7bc394c6535e0ca63d9e18c48949c
Physick <96335032+DegustatorPonos@users.noreply.github.com>
2025-04-10 19:17:41 +0500

    Working build

--- CC/ota.lua
+++ CC/ota.lua
@@ -1,18 +1,47 @@
--- Register apps here.
--- Every app is a dictionary that follows that schema:
--- name = {
+local modem = periferal.find("modem", rednet.open)
+
+-- Set this to trigger updates with password only
+local passw = "12345"
+
+-- Register an app here.
+-- It should contain those fields:
+-- {
     -- FileName: string
     -- URL: string
 -- }
-local Apps = {
-    TestApp = {
-        FileName = "TestApp.lua",
-        URL = "https://raw.githubusercontent.com/DegustatorPonos/CC_OTA/refs/heads/main/programs/HelloWorld.lua",
-    }
+local App = {
+    FileName = "TestApp.lua",
+    URL = "https://pastebin.com/raw/LNBm2kVS",
 }
 
-local function UpdateAllApps()
-    for name, data in pairs(Apps) do 
-        print(name .. "")
+-- Rewirtes an app with the new version
+local function UpdateApp()
+    local resp = http.get(App["URL"])
+    if not resp then return end
+    local file = fs.open(App["FileName"], "w")
+    file.write(resp.readAll())
+end
+
+local function RunApp()
+    os.run({}, App["FileName"])
+end
+
+local function WaitFunc()
+    while true do
+        local id, msg = rednet.receive()
+        if id and msg == passw then
+            return
+        end
     end
 end
+
+-- Entry point
+local function main()
+    while true do
+        UpdateApp()
+        print("App " .. App["FileName"] .. "Updated")
+        parallel.waitForAny(WaitFunc, RunApp)
+    end
+end
+
+main()
--- programs/HelloWorld.lua
+++
@@ -1,9 +0,0 @@
-while true do
-    local ev = os.pullEvent("ota_eol")
-    if not ev then
-        print("Terminated")
-        break
-    end
-    print("Hello, world! from OTA")
-    sleep(1)
-end
