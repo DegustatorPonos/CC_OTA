cc7881b8d13e1ee25d3585f0f2e796e13c485d9f
Physick <96335032+DegustatorPonos@users.noreply.github.com>
2025-04-10 18:13:18 +0500

    Update HelloWorld.lua

--- programs/HelloWorld.lua
+++ programs/HelloWorld.lua
@@ -1,4 +1,9 @@
 while true do
+    local ev = os.pullEvemt("ota_eol")
+    if not ev then
+        print("Terminated")
+        break
+    end
     print("Helllo, world!")
-    sleep(5)
+    sleep(1)
 end
